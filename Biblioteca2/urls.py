68b1b43bf0b9d3cbb7db7343836a58936671df3d
LeoNz09 <160246035+LeoNz09@users.noreply.github.com>
2026-05-21 21:54:19 -0600

    Delete Biblioteca2 directory

--- Biblioteca2/urls.py
+++
@@ -1,7 +0,0 @@
-# urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.pagina_inicio, name='inicio'), 
-]
--- Biblioteca2/views.py
+++
@@ -1,6 +0,0 @@
-# views.py
-from django.shortcuts import render
-
-def pagina_inicio(request):
-    # Aquí podrías poner lógica (ej: contar cuántos libros hay)
-    return render(request, 'inicio.html')
